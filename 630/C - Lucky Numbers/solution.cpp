c425f282f35ecf3ef99bb990ad663d1293fc0093
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-05-09 00:06:32 +0530

    Add Lucky Numbers [C] from Codeforces

---
+++ 630/C - Lucky Numbers/solution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int n;
+    cin >> n;
+ 
+    long long ans = 0;
+    long long power = 2;
+ 
+    for(int i = 1; i <= n; i++) {
+        ans += power;
+        power *= 2;
+    }
+ 
+    cout << ans;
+ 
+    return 0;
+}
