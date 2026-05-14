04d2d074088fc17979fa123c909da3757fc43a0d
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-05-15 02:22:13 +0530

    Add Minimum LCM [M] from Codeforces

---
+++ 1765/M - Minimum LCM/solution.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        int n;
+        cin >> n;
+ 
+        int a = 1;
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                a = n / i;
+                break;
+            }
+        }
+ 
+        cout << a << " " << n - a << '
+';
+    }
+ 
+    return 0;
+}
