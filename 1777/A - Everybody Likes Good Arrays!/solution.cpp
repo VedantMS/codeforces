df8100d572a3b37afd6981e1b80781ee9feaad47
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-06-04 15:08:12 +0530

    Add Everybody Likes Good Arrays! [A] from Codeforces

---
+++ 1777/A - Everybody Likes Good Arrays!/solution.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+            a[i] &= 1;
+        }
+ 
+        int ans = 0;
+        for(int i = 1; i < n; i++) {
+            if(a[i] == a[i - 1]) {
+                ans++;
+            }
+        }
+ 
+        cout << ans << '
+';
+    }
+ 
+    return 0;
+}
