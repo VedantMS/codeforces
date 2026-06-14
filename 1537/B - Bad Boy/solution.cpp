2c2485e2128430e88ae32c8bbc979aa2a4673c07
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-06-14 23:43:52 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve() {
+    int n, m, i, j;
+    cin >> n >> m >> i >> j;
+ 
+    cout << 1 << " " << 1 << " " << n << " " << m << '
+';
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
