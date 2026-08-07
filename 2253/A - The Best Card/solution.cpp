a745cb585352da96d533288f28afdd900414feb1
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-08-07 20:15:56 +0530

    Add The Best Card [A] from Codeforces

---
+++ 2253/A - The Best Card/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+ 
+    n++;
+ 
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            cout << "NO 
+";
+            return;
+        }
+    }
+ 
+    cout << "YES 
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
